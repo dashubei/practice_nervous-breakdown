8f72e515502875fb5923ea318ca07445e66ad74b
dashubei <dashubei6@gmail.com>
2024-12-16 21:31:48 +0900

    2024/12/16 一応完成

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-const numbers = [];
-for (let i = 1; i <= 8; i++) {
-  numbers.push(i, i);
-}
-
 const shuffle = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -11,34 +6,58 @@ const shuffle = (array) => {
   return numbers;
 };
 
+const numbers = [...Array(8).keys()].map((i) => [i + 1, i + 1]).flat();
 const shuffledNumbers = shuffle(numbers);
 
 let stackBox = [];
+const NUM_OF_TRIALS = 2;
+
+let correctTimes = 0;
+let faultTimes = 0;
 
 const onCard = (content) => (content.value = content.dataset.num);
+const offCard = (contents) =>
+  contents.forEach((content) => {
+    content.removeAttribute("value");
+  });
 const done = (array) => {
-  array[0].removeEventListener("click", gameStart);
-  array[1].removeEventListener("click", gameStart);
-
+  array.forEach((arr) => {
+    arr.disabled = true;
+    arr.classList.add("done");
+  });
   array[0].disabled = true;
   array[1].disabled = true;
 };
+const resetStackBox = () => (stackBox.length = 0);
+
+const answerText = document.getElementById("js-correct-times");
+const faultText = document.getElementById("js-fault-times");
+
+answerText.textContent = correctTimes;
+faultText.textContent = faultTimes;
 
 const gameStart = (content) => {
+  if (stackBox.length === NUM_OF_TRIALS) return;
   onCard(content);
 
   if (stackBox.length === 0) {
     stackBox.push(content);
-  } else if (stackBox.length !== 0 && stackBox.length < 2) {
+  } else if (stackBox.length < NUM_OF_TRIALS) {
     stackBox.push(content);
+
     if (stackBox[0].dataset.num === stackBox[1].dataset.num) {
-      console.log("Gooooood!!!");
+      correctTimes++;
+      answerText.textContent = correctTimes;
       done(stackBox);
-      stackBox.length = 0;
+      resetStackBox();
     } else {
-      console.log(content);
-      console.log("ハズレ");
-      stackBox.length = 0;
+      faultTimes++;
+      faultText.textContent = faultTimes;
+
+      setTimeout(() => {
+        offCard(stackBox);
+        resetStackBox();
+      }, 500);
     }
   }
 };
